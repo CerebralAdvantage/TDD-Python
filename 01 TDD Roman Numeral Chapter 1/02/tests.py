bc91fb719f827526471f848976c16ccd93718f6b
jim huddle <james.r.huddle@gmail.com>
2018-11-15 15:19:26 -0500

    Empty File

---
+++ 01 TDD Roman Numeral Chapter 1/02/tests.py
@@ -0,0 +1,14 @@
+from rome import add
+
+assert add('I', 'I') == 'II'
+
+"""
+after adding rome.py, empty file:
+
+user0@ii:~/Desktop/git/TDD/TDD-Python/01 TDD Roman Numeral Chapter 1/02$ python tests.py
+Traceback (most recent call last):
+  File "tests.py", line 1, in <module>
+    from rome import add
+ImportError: cannot import name add
+
+"""
