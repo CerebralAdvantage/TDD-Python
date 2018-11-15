e819f598b35f86ff7406d826eaaa6c9078f115d4
jim huddle <james.r.huddle@gmail.com>
2018-11-15 15:16:18 -0500

    No File At All

---
+++ 01 TDD Roman Numeral Chapter 1/01/test.py
@@ -0,0 +1,14 @@
+from rome import add
+
+assert add('I', 'I') == 'II'
+
+
+"""
+with no file named rome.py, at all:
+
+user0@ii:~/Desktop/git/TDD/TDD-Python/01 TDD Roman Numeral Chapter 1/01$ python test.py
+Traceback (most recent call last):
+  File "test.py", line 1, in <module>
+    from rome import add
+ImportError: No module named rome
+"""
